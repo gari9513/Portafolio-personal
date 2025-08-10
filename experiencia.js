60c8f1da648a495d989428eeb007c935e530a148
gari9513 <garcia.ivonne1395@gmail.com>
2025-08-10 17:54:31 -0600

    inicio

--- experiencia.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.habilidades').forEach(boton => {
-  boton.addEventListener('click', () => {
-    const detalles = boton.nextElementSibling;
-    detalles.classList.toggle('oculto');
-    boton.textContent = detalles.classList.contains('oculto') ? 'Habilidades' : 'Ver menos'; 
-  });
-});
